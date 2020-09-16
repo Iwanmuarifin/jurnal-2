fdec5e2b4d8929486e3790fe47b1fdc47f65dd44
Iwanmuarifin <71206515+Iwanmuarifin@users.noreply.github.com>
2020-09-16 20:26:33 +0700

    Add files via upload

---
+++ tugas2/Hellomiami.java
@@ -0,0 +1,9 @@
+public class Hellomiami{
+
+public static void main(String[] args){
+	System.out.println("Nama:Iwan muarifin");
+	System.out.println("Nim:6706200100");
+	System.out.println("D3IF4404");
+	}
+
+}
