88dadb48c6814dd7d2a30a38c7651c92a82c9d82
Iwanmuarifin <71206515+Iwanmuarifin@users.noreply.github.com>
2020-09-21 21:00:46 +0700

    Add files via upload

---
+++ Identifyyourself.java
@@ -0,0 +1,10 @@
+public class Identifyyourself{
+	public static void main(String[] args){
+		String name = "Iwan muarifin";
+		int age = 18;
+		
+		System.out.println("Halo, "+name);
+		System.out.println("Ternyata usiamu baru"+age+"tahun ya!");
+	
+		}
+}
